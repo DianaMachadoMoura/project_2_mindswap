0ff1c9a2a9f88b4710ecdc10f4bd23eb464781b8
Diana Moura <diana.machado@live.com.pt>
2021-08-20 23:39:33 +0100

    [upd] refactor genresArr at carousel.js, added catches, and cleaned console.log

--- js/carousel.js
+++ js/carousel.js
@@ -1,88 +1,25 @@
 $(document).ready(function () {
 
+    $('.carousel').carousel()
     const APIKey = 'api_key=948abbb99b89ebe76dbe27b039bf28a8';
     const APIRequestPrefix = 'https://api.themoviedb.org/3/';
-    const genresArr = {
-        "genres": [
-            {
-                "id": 28,
-                "name": "Action"
-            },
-            {
-                "id": 12,
-                "name": "Adventure"
-            },
-            {
-                "id": 16,
-                "name": "Animation"
-            },
-            {
-                "id": 35,
-                "name": "Comedy"
-            },
-            {
-                "id": 80,
-                "name": "Crime"
-            },
-            {
-                "id": 99,
-                "name": "Documentary"
-            },
-            {
-                "id": 18,
-                "name": "Drama"
-            },
-            {
-                "id": 10751,
-                "name": "Family"
-            },
-            {
-                "id": 14,
-                "name": "Fantasy"
-            },
-            {
-                "id": 36,
-                "name": "History"
-            },
-            {
-                "id": 27,
-                "name": "Horror"
-            },
-            {
-                "id": 10402,
-                "name": "Music"
-            },
-            {
-                "id": 9648,
-                "name": "Mystery"
-            },
-            {
-                "id": 10749,
-                "name": "Romance"
-            },
-            {
-                "id": 878,
-                "name": "Science Fiction"
-            },
-            {
-                "id": 10770,
-                "name": "TV Movie"
-            },
-            {
-                "id": 53,
-                "name": "Thriller"
-            },
-            {
-                "id": 10752,
-                "name": "War"
-            },
-            {
-                "id": 37,
-                "name": "Western"
-            }]
-    };
+    let genresArr;
 
-    $('.carousel').carousel()
+    // GET MOVIE GENRES
+    function getGenres(APIRequestPrefix, APIKey) {
+        const getGenres = '/genre/movie/list?';
+        fetch(APIRequestPrefix + getGenres + APIKey + '&language=en-US')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                genresArr = data.genres;
+            })
+            .catch(error => console.log(error));
+
+    }
+
+    getGenres(APIRequestPrefix, APIKey);
 
     // SEARCHING 'NOW PLAYING' MOVIES
     fetch(APIRequestPrefix + 'movie/now_playing?' + APIKey + '&language=en-US&page=1')
@@ -104,38 +41,21 @@ $(document).ready(function () {
                     $('.carousel-item__overview' + count).html(movie.overview);
                 }
                 if (count == 3) {
-                    return
+                    return;
                 }
-            })
+            });
         })
-
-})
-
-function getGenres(APIRequestPrefix, APIKey) {
-    const getGenres = '/genre/movie/list?';
-    fetch(APIRequestPrefix + getGenres + APIKey + '&language=en-US')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            return data;
-        })
-}
+        .catch(error => console.log(error));
+});
 
 function displayGenres(genresArr, arr) {
     let strinToReturn = '- ';
-    for(genre of arr) {
-        for(let i=0; i < genresArr.genres.length; i++) {
-            if(genre == genresArr.genres[i].id && i < genresArr.genres.length) {
-                console.log(genresArr.genres[i].id, 'genre id');
-                strinToReturn += genresArr.genres[i].name + ' - ';
-            } 
-        }  
+    for (genre of arr) {
+        for (let i = 0; i < genresArr.length; i++) {
+            if (genre == genresArr[i].id && i < genresArr.length) {
+                strinToReturn += genresArr[i].name + ' - ';
+            }
+        }
     }
     return strinToReturn;
 }
-
-
-
-
