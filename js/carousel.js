f1dbc35525ff764c254e1005b1d24165f2e5f9a8
Diana Moura <diana.machado@live.com.pt>
2021-08-21 16:49:58 +0100

    refactoring carousel.js mathods, fished youtube tailer links

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
 
-    $('.carousel').carousel()
-    const APIKey = 'api_key=948abbb99b89ebe76dbe27b039bf28a8';
-    const APIRequestPrefix = 'https://api.themoviedb.org/3/';
-    let genresArr;
+    $('.carousel').carousel();
 
-    getGenres(APIRequestPrefix, APIKey);
-
-    console.log(genresArr);
-    // SEARCHING 'NOW PLAYING' MOVIES
-    getNowPlayingMovies(APIRequestPrefix, APIKey, genresArr); // undefined
+    setCarousel();
 
 });
 
-// GET MOVIE GENRES
-function getGenres(APIRequestPrefix, APIKey) {
+let genresArr;
+const APIKey = 'api_key=948abbb99b89ebe76dbe27b039bf28a8';
+const APIRequestPrefix = 'https://api.themoviedb.org/3/';
+
+async function setCarousel() {
+
+    await getGenres();
+
+    // SEARCHING 'NOW PLAYING' MOVIES
+    let dataNowPlayingMovies = await getNowPlayingMovies();
+}
+
+function getGenres() {
     const getGenres = '/genre/movie/list?';
-    fetch(APIRequestPrefix + getGenres + APIKey + '&language=en-US')
+    return fetch(APIRequestPrefix + getGenres + APIKey + '&language=en-US')
         .then(response => {
             return response.json();
         })
@@ -26,38 +30,16 @@ function getGenres(APIRequestPrefix, APIKey) {
         .catch(error => console.log(error));
 }
 
-function getNowPlayingMovies(APIRequestPrefix, APIKey, genresArr) {
+async function getNowPlayingMovies() {
 
-    fetch(APIRequestPrefix + 'movie/now_playing?' + APIKey + '&language=en-US&page=1')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let count = 0;
-            data.results.forEach(movie => {
-                if (movie.vote_average > '7' && count < 3) {
-                    count++;
-                    $('#carousel__img' + count).css('background-image', 'url(' + 'https://image.tmdb.org/t/p/w500/' + movie.backdrop_path + ')');
-                    $('.carousel-item__poster' + count).attr('src', 'https://image.tmdb.org/t/p/w500/' + movie.poster_path);
-                    $('.carousel-item__title' + count).html(movie.original_title);
-                    const dateYear = movie.release_date.split('-')[0];
-                    $('.carousel-item__date' + count).html(dateYear);
-                    $('.carousel-item__genres' + count).html(displayGenres(genresArr, movie.genre_ids));
-                    $('.carousel-item__rating' + count).html(movie.vote_average);
-                    $('.carousel-item__overview' + count).html(movie.overview);
-                    const youtubeKey = getYoutubeKey(movie.id, APIRequestPrefix, APIKey);
-                    console.log(youtubeKey);
-                    $('.carousel-item__trailer-button' + count).attr('onclick',);
-                }
-                if (count == 3) {
-                    return;
-                }
-            });
-        })
-        .catch(error => console.log(error));
+    const response = await fetch(APIRequestPrefix + 'movie/now_playing?' + APIKey + '&language=en-US&page=1');
+    const data = await response.json();
+
+    await populateCarousel(data);
+
 }
 
-function displayGenres(genresArr, arr) {
+function displayGenres(arr) {
     let strinToReturn = '- ';
     for (genre of arr) {
         for (let i = 0; i < genresArr.length; i++) {
@@ -69,18 +51,44 @@ function displayGenres(genresArr, arr) {
     return strinToReturn;
 }
 
-function getYoutubeKey(id, APIRequestPrefix, APIKey) {
-    fetch(APIRequestPrefix + `/movie/${id}/videos?` + APIKey)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            for (video of data.results) {
-                if (video.name.toLowerCase().includes('trailer')) {
-                    console.log(video.key)
-                    return video.key;
-                }
-            }
-        })
-        .catch(error => console.log(error));
+async function populateCarousel(data) {
+
+    let count = 0;
+    for (movie of data.results) {
+        if (movie.vote_average > '7' && count < 3) {
+            count++;
+            $('#carousel__img' + count).css('background-image', 'url(' + 'https://image.tmdb.org/t/p/w500/' + movie.backdrop_path + ')');
+            $('.carousel-item__poster' + count).attr('src', 'https://image.tmdb.org/t/p/w500/' + movie.poster_path);
+            $('.carousel-item__title' + count).html(movie.original_title);
+            const dateYear = movie.release_date.split('-')[0];
+            $('.carousel-item__date' + count).html(dateYear);
+            $('.carousel-item__genres' + count).html(displayGenres(movie.genre_ids));
+            $('.carousel-item__rating' + count).html(movie.vote_average);
+            $('.carousel-item__overview' + count).html(movie.overview);
+            let youtubeKey = await getYoutubeKey(movie.id);
+            console.log(youtubeKey);
+            $('.carousel-item__trailer-button' + count).attr('onclick',`window.open('https://www.youtube.com/watch?v=${youtubeKey}?start=5&autoplay=1', '_blank')`);
+        }
+        if (count == 3) {
+            return;
+        }
+    }
+}
+
+async function getYoutubeKey(id) {
+
+    const response = await fetch(APIRequestPrefix + `/movie/${id}/videos?` + APIKey)
+    let videoData = await response.json();
+    let videoKey = await getVideo(videoData.results);
+
+    return videoKey;
+}
+
+function getVideo(videoData) {
+    for (video of videoData) {
+        if (video.name.toLowerCase().includes('trailer')) {
+            // console.log(video.key)
+            return video.key;
+        }
+    }
 }
