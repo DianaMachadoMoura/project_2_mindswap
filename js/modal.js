ebc6c900ec7f287175728f4d17a6e703c6385e40
Diana Moura <diana.machado@live.com.pt>
2021-08-24 18:04:53 +0100

    [upd] created showDetails method, to add content to modal

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,10 @@
 var modal = document.getElementById("mmodal-wrapper");
 
 // Get the modal
-windows.onload = () => {
+
 
 
     var modal = document.getElementById("modal-wrapper");
-
-    console.log(modal);
 
     // // Get the button that opens the modal
     // var btn = document.querySelectorAll(".myBtn");
@@ -23,9 +21,9 @@ windows.onload = () => {
             modal.style.display = "none";
         }
     }
-}
 
-function showDetails(id) {
+
+async function showDetails(id) {
 
     var modal = document.getElementById("modal-wrapper");
 
@@ -36,7 +34,33 @@ function showDetails(id) {
             movieToShow = movie;
         }
     }
-    console.log(movieToShow.title);
+
+    
+    let posterElement = document.querySelector('.modal-content__poster');
+    posterElement.src = imgPath + movieToShow.poster_path;
+    
+    let title = document.querySelector('.modal-content__title');
+    title.innerHTML = movieToShow.title;
+
+    let releaseYear = document.querySelector('.modal-content__year');
+    releaseYear.innerHTML = movieToShow.release_date.split('-')[0]
+    ;
+
+    let genres = document.querySelector('.modal-content__genres');
+    genres.innerHTML = displayGenres(movieToShow.genre_ids);
+
+    let movieRating = document.querySelector('.modal-content__rating');
+    movieRating.innerHTML = movieToShow.vote_average;
+    
+    let movieOverview = document.querySelector('.modal-content__overview');
+    movieOverview.innerHTML = movieToShow.overview;
+
+    let movieTrailerBtn = document.querySelector('.modal-content__trailer-button');
+    let videoKey = await getYoutubeKey(movieToShow.id);
+    
+    movieTrailerBtn.setAttribute('onclick',`window.open('https://www.youtube.com/watch?v=${videoKey}?start=5&autoplay=1', '_blank')`);
+
+
 }
 
 function closeModal() {
